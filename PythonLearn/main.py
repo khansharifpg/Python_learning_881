4e28de6e5aa03f8c4534e1fa7bee2faece6cd98f
SHARIF AHAMED KHAN <47912154+khansharifpg@users.noreply.github.com>
2025-06-27 17:54:15 +0930

    Add files via upload

---
+++ PythonLearn/main.py
@@ -0,0 +1,6 @@
+print("Hello Khan")
+print("How yeah doing")
+print(3)
+
+#if(a>3)
+
---
+++ PythonLearn/variable/main.py
@@ -0,0 +1,12 @@
+age= 34 #integer
+name= "Sharif"
+cgpa= 4.55 #float
+
+print(age)
+print(type(age))
+print(name)
+print(type(name))
+
+is_completed= True
+print(is_completed)
+print(type(is_completed))
